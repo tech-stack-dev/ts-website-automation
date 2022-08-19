5c43f69181a1c4d1ee23140b82096442cea1ae81
Vitalii Dolotov <vitaliy.dolotov@gmail.com>
2022-08-19 18:14:15 +0300

    Removed not used code from StringExtensions

--- PlaywrightAutomation/Extensions/StringExtensions.cs
+++ PlaywrightAutomation/Extensions/StringExtensions.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-using ChoETL;
+﻿using ChoETL;
 
 namespace PlaywrightAutomation.Extensions
 {
@@ -10,17 +7,6 @@ namespace PlaywrightAutomation.Extensions
         public static string ToAutomationValue(this string str)
         {
             return str?.ToPascalCase().Replace(" ", string.Empty);
-        }
-
-        public static string RemoveSpaceAndSlash(this string str)
-        {
-            return str.Replace(" ", string.Empty).Replace("/", string.Empty);
-        }
-
-        public static List<string> ListTagsWithoutNumber(this List<string> strList)
-        {
-            var tagsList = strList.Select(x => Regex.Matches(x, @"^[a-zA-Z\s/]+\b"));
-            return tagsList.SelectMany(x => x).Select(x => x.Value).ToList();
         }
     }
 }
