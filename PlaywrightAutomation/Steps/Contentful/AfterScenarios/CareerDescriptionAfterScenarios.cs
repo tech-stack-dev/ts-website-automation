499ecc839641385995644536ac624bed01472175
Vitalii Dolotov <vitaliy.dolotov@gmail.com>
2022-08-25 16:46:00 +0300

    Refactoring of Career afterscenarios

--- PlaywrightAutomation/Steps/Contentful/AfterScenarios/CareerDescriptionAfterScenarios.cs
+++ PlaywrightAutomation/Steps/Contentful/AfterScenarios/CareerDescriptionAfterScenarios.cs
@@ -7,21 +7,19 @@ using AutomationUtils.Utils;
 namespace PlaywrightAutomation.Steps.Contentful
 {
     [Binding]
-    public class AfterScenarios : SpecFlowContext
+    public class CareerDescriptionAfterScenarios : SpecFlowContext
     {
         private readonly ContentfulClient _contentfulClient;
         private readonly CreatedCareerDescription _createdCareerDescriptions;
-        private readonly CreatedCareer _createdCareer;
 
-        public AfterScenarios(ContentfulClient contentfulClient, CreatedCareerDescription createdCareerDescriptions, CreatedCareer createdCareer)
+        public CareerDescriptionAfterScenarios(ContentfulClient contentfulClient, CreatedCareerDescription createdCareerDescriptions)
         {
             _contentfulClient = contentfulClient;
             _createdCareerDescriptions = createdCareerDescriptions;
-            _createdCareer = createdCareer;
         }
 
         [AfterScenario("Cleanup")]
-        public void UnpublishAndDeleteCreatedCareerDescriptionsAndCareer()
+        public void UnpublishAndDeleteCreatedCareerDescriptions()
         {
             if (!_createdCareerDescriptions.Value.Any())
                 return;
@@ -44,30 +42,6 @@ namespace PlaywrightAutomation.Steps.Contentful
                 catch
                 {
                     Logger.Write($"Error deleting '{career.TitleUs}' career", Logger.LogLevel.Warning);
-                }
-            }
-
-            if (!_createdCareer.Value.Any())
-                return;
-
-            foreach (var career in _createdCareer.Value)
-            {
-                try
-                {
-                    _contentfulClient.UnpublishCareer(career);
-                }
-                catch
-                {
-                    Logger.Write($"Error unpublishing '{career.NameUs}' career", Logger.LogLevel.Warning);
-                }
-
-                try
-                {
-                    _contentfulClient.DeleteCareer(career);
-                }
-                catch
-                {
-                    Logger.Write($"Error deleting '{career.NameUs}' career", Logger.LogLevel.Warning);
                 }
             }
         }
