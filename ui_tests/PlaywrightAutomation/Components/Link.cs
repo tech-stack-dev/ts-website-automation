acc7c6730e73fbf8437eb55cab606d4258b9e4e7
Danya <danya.sydortsov@tech-stack.io>
2023-01-05 18:23:26 +0200

    Add 'TSWEB-302-Consulting-Service' pull request from old repo

---
+++ ui_tests/PlaywrightAutomation/Components/Link.cs
@@ -0,0 +1,12 @@
+﻿namespace PlaywrightAutomation.Components
+{
+    // Rework component after developers add data-id
+    public class Link : BaseWebComponent
+    {
+        public override string Construct()
+        {
+            var selector = "//img[@class='arrow']";
+            return selector;
+        }
+    }
+}
---
+++ ui_tests/PlaywrightAutomation/Pages/OurServices/ConsultingService/RelatedServicesSectionPage.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Playwright;
+
+namespace PlaywrightAutomation.Pages.OurServices.ConsultingService
+{
+    internal class RelatedServicesSection : BasePage, IWebContainer
+    {
+        public string Container => "//div[@id='consulting-related-services-section']";
+
+        // Use with 'ContentBlock' method
+        public string BlockNames => "//h3";
+
+        public ILocator ContentBlock(string blockName)
+        {
+            return Page.Locator(Container).Locator($"//h3[contains(text(),'{blockName}')]//..");
+        }
+    }
+}
