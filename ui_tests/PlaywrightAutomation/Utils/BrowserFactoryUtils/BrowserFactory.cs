0fa7b156a597e4fda6bf373eb8933342f30a8f2a
Vitalii Dolotov <vitaliy.dolotov@gmail.com>
2023-01-23 18:15:20 +0200

    Removed browser config from appsettings

--- ui_tests/PlaywrightAutomation/Utils/BrowserFactoryUtils/BrowserFactory.cs
+++ ui_tests/PlaywrightAutomation/Utils/BrowserFactoryUtils/BrowserFactory.cs
@@ -38,7 +38,7 @@ namespace PlaywrightAutomation.Utils
         {
             var options = new BrowserTypeLaunchOptions
             {
-                Headless = BrowserConfigProvider.Headless
+                Headless = false
             };
 
             _browser = PlaywrightInstance.Chromium.LaunchAsync(options).GetAwaiter().GetResult();
