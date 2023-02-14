18b78019e6a872045eefe7583b2f44569b99c32a
Anastasiia Liashova <anastasiia.liashova@tech-stack.io>
2023-02-15 01:17:14 +0200

    code fixed

--- ui_tests_playwright/automation/test/api/API.test.ts
+++ ui_tests_playwright/automation/test/api/API.test.ts
@@ -5,15 +5,13 @@ import BaseApiSteps from "../../base/step/BaseApiSteps";
 import { ClientsEnum } from "../../base/client/ClientsEnum";
 import RequestOptions from "../../base/client/RequestOptions";
 
-let apiSteps: ApiSteps = new ApiSteps();
-
 test.beforeEach(async () => {
-    await new BaseApiSteps().createClient(ClientsEnum.Client_1);
+    await BaseApiSteps.createClient(ClientsEnum.Client_1);
 });
 
 test("Api test GET", async () => {
-    await apiSteps.executeGetRequest("/api/users?page=2")
-    await apiSteps.checkPropertyValue("page", 2);
+    await ApiSteps.executeGetRequest("/api/users?page=2")
+    await ApiSteps.checkPropertyValue("page", 2);
 });
 
 test("Api test POST", async () => {
@@ -25,6 +23,6 @@ test("Api test POST", async () => {
     let requestOptions: RequestOptions = new RequestOptions();
     requestOptions.data = userDtoVariable.value;
 
-    await apiSteps.executePostRequest("/api/users", requestOptions, 201);
-    await apiSteps.checkPropertyValue("name", "morpheus");
+    await ApiSteps.executePostRequest("/api/users", requestOptions, 201);
+    await ApiSteps.checkPropertyValue("name", "morpheus");
 });
