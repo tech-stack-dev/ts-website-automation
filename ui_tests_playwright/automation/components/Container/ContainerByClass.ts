cd464c98815a950004fe1d0ecd9c8d28338e4442
Daniil Vostrikov <Daniil.Vostrikov@juriba.com>
2023-03-06 16:30:17 +0200

    remove unused class

--- ui_tests_playwright/automation/components/Container/ContainerByClass.ts
+++ ui_tests_playwright/automation/components/Container/ContainerByClass.ts
@@ -1,7 +1,7 @@
 import { Locator, Page } from "@playwright/test";
 import BaseComponent from "../../base/component/BaseComponent";
 
-export default class ContainerById extends BaseComponent {
+export default class ContainerByClass extends BaseComponent {
     constructor(page: Page, identifier: string, parent?: Locator) {
         super(page, identifier, parent);
         this.ComponentContext = `//div[contains(@class, "${identifier}")]`;
