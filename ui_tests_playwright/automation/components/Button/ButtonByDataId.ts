f539c3a3a5af5283b0933146b68b74a9e4d9dc89
YevheniiKel <68158472+YevheniiKel@users.noreply.github.com>
2023-03-09 12:40:51 +0200

    deleted comments

--- ui_tests_playwright/automation/components/Button/ButtonByDataId.ts
+++ ui_tests_playwright/automation/components/Button/ButtonByDataId.ts
@@ -5,7 +5,6 @@ export default class ButtonByDataId extends BaseComponent {
     constructor(page: Page, identifier: string, parent?: Locator) {
         super(page, identifier, parent);
         // Class value can contain as 'button' as 'Button' value 
-        // controls should be in separate class!
         this.ComponentContext = `//*[@data-id='${identifier}']`;
     }
 }
