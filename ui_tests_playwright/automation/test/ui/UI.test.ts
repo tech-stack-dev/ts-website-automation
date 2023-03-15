5b762860e0352406962de69a4204349805096179
YevheniiKel <68158472+YevheniiKel@users.noreply.github.com>
2023-03-15 18:25:55 +0200

    revert merge from master1

---
+++ ui_tests_playwright/automation/test/ui/UI.test.ts
@@ -0,0 +1,24 @@
+import { test } from "@playwright/test";
+import { driver } from "../../base/driver/Driver";
+import { baseDriverSteps } from "../../base/step/BaseDriverSteps";
+import ButtonByDataId from "../../components/Button/ButtonByDataId";
+import AboutUs from "../../identifiers/AboutUs";
+import Button from "../../identifiers/Button";
+import UrlProvider from "../../providers/UrlProvider";
+import { buttonSteps } from "../../steps/components/Button/ButtonSteps";
+import { containerSteps } from "../../steps/components/Container/ContainerSteps";
+
+test.beforeEach(async () => {
+    await baseDriverSteps.createsNewBrowser();
+    await baseDriverSteps.goToUrl(UrlProvider.careerUrl());
+    await buttonSteps.clickButton(ButtonByDataId, Button.NavigationTab_AboutUs);
+});
+
+test("Test check 'We are Techstack' section", async () => {
+    await containerSteps.checkContainerNumber(AboutUs.WeAreTechstackId, "01");
+    await containerSteps.checkContainerTitle(AboutUs.WeAreTechstackId, "We are Techstack");
+});
+
+test.afterEach(async () => {
+    await driver.closeDrivers();
+})
