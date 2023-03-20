84a5cd6786af69a08faa4d8069ada580f96f8bf7
Daniil Vostrikov <Daniil.Vostrikov@juriba.com>
2023-03-20 18:40:36 +0200

    CheckLocalizationOnJobPage moved to TS solution

---
+++ ui_tests_playwright/automation/preconditionsData/uiPreconditions/JobPagePreconditions.ts
@@ -0,0 +1,11 @@
+export default class JobPagePreconditions {
+	public static descriptionBlocksIdentifiersAndUaHeaders: Map<string, string> = new Map([
+        ['role', 'Про роль:'],
+        ['skills', 'Ти маєш:'],
+        ['about_us', 'Ми маємо:'],
+        ['description', 'Про продукт:'],
+        ['responsibilities', 'Ти будеш:'],
+        ['our_benefits','Ми будемо:']
+    ])
+
+}
--- ui_tests_playwright/playwright.config.ts
+++ ui_tests_playwright/playwright.config.ts
@@ -1,5 +1,4 @@
 import type {PlaywrightTestConfig} from '@playwright/test';
-import {devices} from '@playwright/test';
 import dotenv from 'dotenv';
 import path from 'path';
 
