bc84c6aeec00d1869c1d083304325e300de50d66
Anastasiia Liashova <anastasiia.liashova@tech-stack.io>
2023-03-27 22:31:29 +0300

    TSWEB-150 added tests to check the carousel buttons

--- ui_tests_playwright/automation/identifiers/AboutUs.ts
+++ ui_tests_playwright/automation/identifiers/AboutUs.ts
@@ -4,7 +4,7 @@ export default class AboutUs {
 
 	static OurHistoryTitle: string = 'SectionTitle-OurHistoryTitle';
 	static OurHistoryNumber: string = 'SectionNumber-02Number';
-	static OurHistoryCarosel: string = 'CarouselInfoLineBlock-OurHistory';
+	static OurHistoryCarousel: string = 'CarouselInfoLineBlock-OurHistory';
 
 	static OurAchievementsTitle: string = 'SectionTitle-OurAchievementsTitle';
 	static OurAchievementsNumber: string = 'SectionNumber-03Number';
@@ -15,6 +15,7 @@ export default class AboutUs {
 	static TechstackRolesId: string = 'techstackRoles';
 	static TechstackRolesTitle: string = 'SectionTitle-TechstackRolesTitle';
 	static TechstackRolesNumber: string = 'SectionNumber-05Number';
+
 	static Roles_Mentor: string = 'Mentor';
 	static Roles_TechExpert: string = 'Tech\\ Expert';
 	static Roles_PreSaleExpert: string = 'Pre-Sale\\ Expert';
@@ -22,9 +23,10 @@ export default class AboutUs {
 	static Roles_VPofFunction: string = 'VP\\ of\\ Function';
 
 	static EngineeringCultureId: string = 'engineeringCulture';
-	static EngineeringCultureTitle: string =
-		'SectionTitle-EngineeringCultureTitle';
+	static EngineeringCultureTitle: string = 'SectionTitle-EngineeringCultureTitle';
 	static EngineeringCultureNumber: string = 'SectionNumber-06Number';
+	static EngineeringCultureCarousel: string = 'CarouselPhotoBlock-EngineeringCulture';
+
 	static EC_TechClubs: string = 'Tech\\ clubs';
 	static EC_Meetups: string = 'Meetups';
 	static EC_TechCompetitions: string = 'Tech\\ Competitions';
@@ -33,17 +35,22 @@ export default class AboutUs {
 	static EC_LoyaltyProgram: string = 'Loyalty\\ Program';
 
 	static SocialResponsibilityId: string = 'socialResponsibility';
-	static SocialResponsibilityTitle: string =
-		'SectionTitle-SocialResponsibilityTitle';
+	static SocialResponsibilityTitle: string = 'SectionTitle-SocialResponsibilityTitle';
 	static SocialResponsibilityNumber: string = 'SectionNumber-07Number';
+	static SocialResponsibilityCarousel: string = 'CarouselPhotoBlock-SocialResponsibility';
+
 	static SR_Charity = 'Charity';
 	static SR_EnvironmentalSafety = 'Environmental\\ Safety';
 	static SR_EducationSupport = 'Education\\ Support';
 
 	static CandidatePathTitle: string = 'SectionTitle-CandidatePathTitle';
 	static CandidatePathNumber: string = 'SectionNumber-08Number';
-	static CandidateCarosel: string = 'CarouselInfoLineBlock-CandidatePath';
+	static CandidateCarousel: string = 'CarouselInfoLineBlock-CandidatePath';
 
-	static CaroselBlock: string = 'ContentLineBlock-ContentLineBlock';
+	static CarouselBlock: string = 'ContentLineBlock-ContentLineBlock';
+	static CarouselPrevButton: string = 'CarouselPrevButton-ControlButtonsPrev';
+	static CarouselNextButton: string = 'CarouselNextButton-ControlButtonsNext';
+
+	static ApplyTitle: string = 'apply-title';
 	static ApplyNowButton = 'ApplyNowVacancyButton-ProposalMessage';
 }
--- ui_tests_playwright/automation/test/ui/aboutUs/AboutUsBlock.test.ts
+++ ui_tests_playwright/automation/test/ui/aboutUs/AboutUsBlock.test.ts
@@ -38,27 +38,27 @@ test("Check the section title and number from the 'AboutUs' block @Regression @A
 });
 
 test("Check the 'Our History' carousel from the 'AboutUs' block @Regression @AboutUs @TSWEB-150", async () => {
-    let carosel = driver.getByTestId(AboutUs.OurHistoryCarosel);
+    let carousel = driver.getByTestId(AboutUs.OurHistoryCarousel);
 
-    await expect(carosel.getByTestId(AboutUs.CaroselBlock).nth(0)).toContainText('01 2014');
-    await expect(carosel.getByTestId(AboutUs.CaroselBlock).nth(1)).toContainText('02 2015');
-    await expect(carosel.getByTestId(AboutUs.CaroselBlock).nth(2)).toContainText('03 2016');
-    await expect(carosel.getByTestId(AboutUs.CaroselBlock).nth(3)).toContainText('04 2017');
-    await expect(carosel.getByTestId(AboutUs.CaroselBlock).nth(4)).toContainText('05 2018');
-    await expect(carosel.getByTestId(AboutUs.CaroselBlock).nth(5)).toContainText('06 2019');
-    await expect(carosel.getByTestId(AboutUs.CaroselBlock).nth(6)).toContainText('07 2020');
-    await expect(carosel.getByTestId(AboutUs.CaroselBlock).nth(7)).toContainText('08 2021');
+    await expect(carousel.getByTestId(AboutUs.CarouselBlock).nth(0)).toContainText('01 2014');
+    await expect(carousel.getByTestId(AboutUs.CarouselBlock).nth(1)).toContainText('02 2015');
+    await expect(carousel.getByTestId(AboutUs.CarouselBlock).nth(2)).toContainText('03 2016');
+    await expect(carousel.getByTestId(AboutUs.CarouselBlock).nth(3)).toContainText('04 2017');
+    await expect(carousel.getByTestId(AboutUs.CarouselBlock).nth(4)).toContainText('05 2018');
+    await expect(carousel.getByTestId(AboutUs.CarouselBlock).nth(5)).toContainText('06 2019');
+    await expect(carousel.getByTestId(AboutUs.CarouselBlock).nth(6)).toContainText('07 2020');
+    await expect(carousel.getByTestId(AboutUs.CarouselBlock).nth(7)).toContainText('08 2021');
 });
 
 test("Check the 'Candidate Path' carousel from the 'AboutUs' block @Regression @AboutUs @TSWEB-150", async () => {
-    let carosel = driver.getByTestId(AboutUs.CandidateCarosel);
+    let carousel = driver.getByTestId(AboutUs.CandidateCarousel);
 
-    await expect(carosel.getByTestId(AboutUs.CaroselBlock).nth(0)).toContainText('01 CV');
-    await expect(carosel.getByTestId(AboutUs.CaroselBlock).nth(1)).toContainText('02 Pre-screening');
-    await expect(carosel.getByTestId(AboutUs.CaroselBlock).nth(2)).toContainText('03 Test task');
-    await expect(carosel.getByTestId(AboutUs.CaroselBlock).nth(3)).toContainText('04 Tech expert review');
-    await expect(carosel.getByTestId(AboutUs.CaroselBlock).nth(4)).toContainText('05 Tech expert interview');
-    await expect(carosel.getByTestId(AboutUs.CaroselBlock).nth(5)).toContainText('06 Product owner interview');
+    await expect(carousel.getByTestId(AboutUs.CarouselBlock).nth(0)).toContainText('01 CV');
+    await expect(carousel.getByTestId(AboutUs.CarouselBlock).nth(1)).toContainText('02 Pre-screening');
+    await expect(carousel.getByTestId(AboutUs.CarouselBlock).nth(2)).toContainText('03 Test task');
+    await expect(carousel.getByTestId(AboutUs.CarouselBlock).nth(3)).toContainText('04 Tech expert review');
+    await expect(carousel.getByTestId(AboutUs.CarouselBlock).nth(4)).toContainText('05 Tech expert interview');
+    await expect(carousel.getByTestId(AboutUs.CarouselBlock).nth(5)).toContainText('06 Product owner interview');
 });
 
 test("Check the 'Techstack roles', 'Engineering Culture' and 'Social Responsibility' blocks from the 'AboutUs' block @Regression @AboutUs @TSWEB-150", async () => {
@@ -85,9 +85,99 @@ test("Check the 'Techstack roles', 'Engineering Culture' and 'Social Responsibil
     await expect(socialResponsibility.locator(`#${AboutUs.SR_EducationSupport}`)).toContainText('Education Support');
 });
 
-test("Check the redirect to the 'Jobs' block from the 'AboutUs' block @Regression @AboutUs @TSWEB-150", async () => {
+test("Check the 'Apply' block from the 'AboutUs' block @Regression @AboutUs @TSWEB-150", async () => {
+    await expect(driver.locator(`#${AboutUs.ApplyTitle}`)).toContainText('Would you like to be a part of our team?');
+    
     await driver.getByTestId(AboutUs.ApplyNowButton).click();
     await baseDriverSteps.checkUrl(UrlProvider.careerUrl());
+});
+
+test("Check the buttons of the photo carousel from the 'AboutUs' block @Regression @AboutUs @TSWEB-150", async () => {
+    let engineeringCultureCarousel = driver.getByTestId(AboutUs.EngineeringCultureCarousel);
+    let engineeringCulturePrevButton = engineeringCultureCarousel.getByTestId(AboutUs.CarouselPrevButton);
+    let engineeringCultureNextButton = engineeringCultureCarousel.getByTestId(AboutUs.CarouselNextButton);
+
+    await expect(engineeringCulturePrevButton).toHaveAttribute('data-disabled', 'true');
+    await expect(engineeringCultureNextButton).toHaveAttribute('data-disabled', 'false');
+
+    await engineeringCultureNextButton.click();
+
+    await expect(engineeringCulturePrevButton).toHaveAttribute('data-disabled', 'false');
+    await expect(engineeringCultureNextButton).toHaveAttribute('data-disabled', 'false');
+
+    let engineeringCultureCounter = 0;
+    while (engineeringCultureCounter < 3) {
+        await engineeringCultureNextButton.click();
+        engineeringCultureCounter++;
+    }
+
+    await expect(engineeringCulturePrevButton).toHaveAttribute('data-disabled', 'false');
+    await expect(engineeringCultureNextButton).toHaveAttribute('data-disabled', 'true');
+
+    let socialResponsibilityCarousel = driver.getByTestId(AboutUs.SocialResponsibilityCarousel);
+    let socialResponsibilityPrevButton = socialResponsibilityCarousel.getByTestId(AboutUs.CarouselPrevButton);
+    let socialResponsibilityNextButton = socialResponsibilityCarousel.getByTestId(AboutUs.CarouselNextButton);
+
+    await expect(socialResponsibilityPrevButton).toHaveAttribute('data-disabled', 'true');
+    await expect(socialResponsibilityNextButton).toHaveAttribute('data-disabled', 'false');
+
+    await socialResponsibilityNextButton.click();
+
+    await expect(socialResponsibilityPrevButton).toHaveAttribute('data-disabled', 'false');
+    await expect(socialResponsibilityNextButton).toHaveAttribute('data-disabled', 'false');
+
+    let socialResponsibilityCounter = 0;
+    while (socialResponsibilityCounter < 3) {
+        await socialResponsibilityNextButton.click();
+        socialResponsibilityCounter++;
+    }
+
+    await expect(socialResponsibilityPrevButton).toHaveAttribute('data-disabled', 'false');
+    await expect(socialResponsibilityNextButton).toHaveAttribute('data-disabled', 'true');
+});
+
+test("Check the buttons of the info carousel from the 'AboutUs' block @Regression @AboutUs @TSWEB-150", async () => {
+    let ourHistoryCarousel = driver.getByTestId(AboutUs.OurHistoryCarousel);
+    let ourHistotyPrevButton = ourHistoryCarousel.getByTestId(AboutUs.CarouselPrevButton);
+    let ourHistoryNextButton = ourHistoryCarousel.getByTestId(AboutUs.CarouselNextButton);
+
+    await expect(ourHistotyPrevButton).toHaveAttribute('data-disabled', 'true');
+    await expect(ourHistoryNextButton).toHaveAttribute('data-disabled', 'false');
+
+    await ourHistoryNextButton.click();
+
+    await expect(ourHistotyPrevButton).toHaveAttribute('data-disabled', 'false');
+    await expect(ourHistoryNextButton).toHaveAttribute('data-disabled', 'false');
+
+    let ourHistoryCounter = 0;
+    while (ourHistoryCounter < 6) {
+        await ourHistoryNextButton.click();
+        ourHistoryCounter++;
+    }
+
+    await expect(ourHistotyPrevButton).toHaveAttribute('data-disabled', 'false');
+    await expect(ourHistoryNextButton).toHaveAttribute('data-disabled', 'true');
+
+    let candidateCarousel = driver.getByTestId(AboutUs.CandidateCarousel);
+    let candidatePrevButton = candidateCarousel.getByTestId(AboutUs.CarouselPrevButton);
+    let candidateNextButton = candidateCarousel.getByTestId(AboutUs.CarouselNextButton);
+
+    await expect(candidatePrevButton).toHaveAttribute('data-disabled', 'true');
+    await expect(candidateNextButton).toHaveAttribute('data-disabled', 'false');
+
+    await candidateNextButton.click();
+
+    await expect(candidatePrevButton).toHaveAttribute('data-disabled', 'false');
+    await expect(candidateNextButton).toHaveAttribute('data-disabled', 'false');
+
+    let candidateCounter = 0;
+    while (candidateCounter < 4) {
+        await candidateNextButton.click();
+        candidateCounter++;
+    }
+
+    await expect(candidatePrevButton).toHaveAttribute('data-disabled', 'false');
+    await expect(candidateNextButton).toHaveAttribute('data-disabled', 'true');
 });
 
 
