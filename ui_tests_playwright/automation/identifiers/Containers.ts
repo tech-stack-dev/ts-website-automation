196a6f04ad6cf62b19175a89f0206e59b586f3be
YevheniiKel <68158472+YevheniiKel@users.noreply.github.com>
2023-04-10 01:10:34 +0300

    added test, changed setup steps in the test file

--- ui_tests_playwright/automation/identifiers/Containers.ts
+++ ui_tests_playwright/automation/identifiers/Containers.ts
@@ -1,6 +1,11 @@
 export default class Containers {
 	static navigationHeaderClass = 'styledComponents__LogoHeaderWrapper';
+	static standWithUkraineClass = 'stand-title';
 	static careerCardWithoutModifier = 'CardWrapper-';
+
+	// Blog
+	static postTitileClass = 'post-title';
+
 	// Job page
 	static jobPageApplyProposition = 'styledComponents__ApplyPropositionWrapper';
 	static jobPageHeaderWrapper = 'styledComponents__NavigationHeaderWrapper';
--- ui_tests_playwright/automation/test/ui/navigationHeader/NavigationHeaderTests.test.ts
+++ ui_tests_playwright/automation/test/ui/navigationHeader/NavigationHeaderTests.test.ts
@@ -8,7 +8,8 @@ import Containers from '../../../identifiers/Containers';
 import Button from '../../../identifiers/Button';
 
 test.beforeEach(async () => {
-	await baseDriverSteps.createsNewBrowserAndGoToUrl(UrlProvider.careerUrl());
+	await baseDriverSteps.createsNewBrowser();
+	await baseDriverSteps.goToUrl(UrlProvider.careerUrl());
 });
 
 test('Check that user can switch language in navigation header @Regression @NavigationHeader @TSWEB-560', async () => {
@@ -16,6 +17,29 @@ test('Check that user can switch language in navigation header @Regression @Navi
 	const uaButtonSwitcher = headerContainer.Element.getByTestId(Button.UaLanguageSwitcher);
 	await uaButtonSwitcher.click();
 	await expect(uaButtonSwitcher).toHaveClass(/active-locale/);
+});
+
+test('Check the the "Stand with Ukraine" block with localization @Regression @StandWithUkraine @TSWEB-132', async () => {
+	const buttonSwitcher = await (
+		await containerSteps.getContainer(ContainerByClass, Containers.navigationHeaderClass)
+	).Element.getByTestId(Button.UaLanguageSwitcher);
+	expect((await containerSteps.getContainer(ContainerByClass, Containers.standWithUkraineClass)).Element).toHaveText(
+		'Techstack stands with Ukraine'
+	);
+	expect(await driver.getByTestId('LearnMoreButton-SharedLearnMore2')).toHaveText('Learn More');
+
+	await buttonSwitcher.click();
+	expect((await containerSteps.getContainer(ContainerByClass, Containers.standWithUkraineClass)).Element).toHaveText(
+		'Відповідь Techstack на війну в Україні',
+		{timeout: 5000}
+	);
+	expect(await driver.getByTestId('LearnMoreButton-SharedLearnMore2')).toHaveText('Ознайомитися');
+
+	await driver.getByTestId('LearnMoreButton-SharedLearnMore2').click();
+	await baseDriverSteps.checkUrl('https://tech-stack.com/blog/techstack-stands-with-ukraine/');
+	expect((await containerSteps.getContainer(ContainerByClass, Containers.postTitileClass)).Element).toHaveText(
+		'Techstack Stands with Ukraine'
+	);
 });
 
 test.afterEach(async () => {
