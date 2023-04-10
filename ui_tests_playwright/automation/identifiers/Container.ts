18f354a4470f043556f2e84d3b6fc7230cc0d539
Anastasiia Liashova <anastasiia.liashova@tech-stack.io>
2023-04-10 15:04:13 +0300

    TSWEB-603 added tests for QA as a Service block

---
+++ ui_tests_playwright/automation/identifiers/Container.ts
@@ -0,0 +1,14 @@
+export default class Container {
+    static Breadcrumbs = 'Breadcrumbs';
+    static Title = 'Title';
+    static ContainerTitle = 'ContainerTitle';
+    static ContainerNumber = 'ContainerNumber';
+
+    static ContainerSection = 'ContainerSection';
+    static SectionNumber = 'SectionNumber';
+    static SectionTitle = 'SectionTitle';
+
+    static ContainerBlock = 'ContainerBlock';
+    static BlockTitle = 'BlockTitle';
+    static BlockSection = 'BlockSection';
+}
