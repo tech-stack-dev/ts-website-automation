61986778f464a4194e80975b8f84691622c62d97
Anastasiia Liashova <anastasiia.liashova@tech-stack.io>
2023-04-11 10:55:23 +0300

    TSWEB-603 fixed comments

--- ui_tests_playwright/automation/identifiers/Career.ts
+++ ui_tests_playwright/automation/identifiers/Career.ts
@@ -4,8 +4,8 @@ export default class Career {
 	// Job Header
 	static breadcrumbsInCareer = 'breadcrumbs-block';
 	static jobHeaderTitle = 'career-title';
-	static jobHeaderJobsTabUa = 'NavigationTab-Jobs';
-	static jobHeaderAboutUsTabUa = 'NavigationTab-AboutUs';
-	static jobHeaderReviewsTabUa = 'NavigationTab-Rev';
-	static jobHeaderContactUsTabUa = 'NavigationTab-ContactUs';
+	static jobHeaderJobsTab = 'NavigationTab-Jobs';
+	static jobHeaderAboutUsTab = 'NavigationTab-AboutUs';
+	static jobHeaderReviewsTab = 'NavigationTab-Rev';
+	static jobHeaderContactUsTab = 'NavigationTab-ContactUs';
 }
