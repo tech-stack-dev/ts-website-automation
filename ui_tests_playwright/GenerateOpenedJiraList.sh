45e3babd1879af8aa9fb6081ae856dc89008c157
YevheniiKel <68158472+YevheniiKel@users.noreply.github.com>
2023-04-20 20:59:27 +0300

    updated algorithm of getting jira tags form testnames

--- ui_tests_playwright/GenerateOpenedJiraList.sh
+++ ui_tests_playwright/GenerateOpenedJiraList.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get install jq
 
-#JiraAuthToken="Basic cWFfdGVjaHN0YWNrQHRlY2gtc3RhY2suaW86QVRBVFQzeEZmR0YwRTVvb01vd1RXVW1DVFYxNF93 bFR4ZmtwYTdleWV0VmNrel82aXNOczJvMk9aLV8tUFNYN2kxekVUb3dFVUdUUTdyT1dQOHNZdjNz Q3JNVV8zODlEMDhZS1QyWWxheVRNOU4wMnJQbDVyTThYZllHYXBhTF84UVdYckMxZ0stbTlsZFNP RzhIaGNpSXhDY0dmc1RCbjBLU2dqU1kyUVB6WkE0UXBkSDRPUVVZPUY4Q0Q5RUND"
 JiraAuthToken=$1
 allowed_statuses=("Ready for QA" "Testing on Stage" "UI testing" "Done" "Closed")
 
@@ -10,26 +9,17 @@ envBuildStep=$2
 
 # Dive through the test files and extract the Jira test tags
 files=$(find . -name "*.test.ts")
-# jira_tags=()
-# for file in $files; do
-#   echo "Processing file: $file"
-#   while read -r line; do
-#     if [[ $line =~ TSWEB-[0-9]{1,8} ]]; then
-#       tag="${BASH_REMATCH[0]}"
-
-#       jira_tags+=("$tag")
-#     fi
-#   done < "$file"
-# done
-
-
 jira_tags=()
-while read -r line; do
-  tags=$(echo "$line" | grep -o 'TSWEB-[0-9]\{1,8\}')
-  for tag in $tags; do
-    jira_tags+=("$tag")
-  done
-done < <(echo "$files" | xargs cat | grep -E 'test\("[^"]+"' -o)
+for file in $files; do
+  echo "Processing file: $file"
+  while read -r line; do
+      tags=$(echo "$line" | grep -o 'TSWEB-[0-9]\{1,8\}')
+      for tag in $tags; do
+      jira_tags+=("$tag")
+    done
+  done < "$file"
+done
+
 # Remove duplicates from the list
 jira_tags=($(echo "${jira_tags[@]}" | tr ' ' '\n' | sort -u))
 
@@ -37,12 +27,16 @@ jira_tags=($(echo "${jira_tags[@]}" | tr ' ' '\n' | sort -u))
 echo "Checking Jira status for the following tags:"
 printf '%s\n' "${jira_tags[@]}"
 echo
-
 ready_tags=()
 for tag in "${jira_tags[@]}"; do
     url="https://ts-website.atlassian.net/rest/api/3/issue/${tag}"
     echo "Making request to $url"
     status=$(curl "$url" --silent --show-error -H "Authorization: $JiraAuthToken" | jq -r '.fields.status.name')
+    # Check that 
+    if [[ -z $status ]]; then
+        echo "Error: Status is null for tag $tag"
+        exit 1
+    fi
     echo "Status for $tag: $status"
     if [[ ! "${allowed_statuses[@]}" =~ "$status" ]]; then
         ready_tags+=("@$tag")
