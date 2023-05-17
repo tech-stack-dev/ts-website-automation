edcc09600ddae6cf8f553cd49ca3e79e6ae364c9
Anastasiia Liashova <anastasiia.liashova@tech-stack.io>
2023-05-17 15:46:50 +0300

    TSWEB-656 added tests for header

--- ui_tests_playwright/automation/identifiers/Link.ts
+++ ui_tests_playwright/automation/identifiers/Link.ts
@@ -1,5 +1,6 @@
 export default class Link {
 	static Logo = 'Logo-Techstack';
+	static HeaderLogo = 'HeaderLogo-HeaderLogo';
 
 	// Breadcrumbs links
 	static Breadcrumbs_Jobs = 'BreadcrumbsLink-SharedJobs';
---
+++ ui_tests_playwright/automation/preconditionsData/uiPreconditions/Colors.ts
@@ -0,0 +1,3 @@
+export default class Colors{
+    static FFC600 = 'rgb(255, 198, 0)';
+}
