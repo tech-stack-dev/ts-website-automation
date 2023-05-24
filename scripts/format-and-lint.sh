27cf83664f5520c6a26f35e26c19f73f54a71886
Daniil Vostrikov <Daniil.Vostrikov@juriba.com>
2023-05-24 13:20:21 +0300

    test

--- scripts/format-and-lint.sh
+++ scripts/format-and-lint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 . "ui_tests_playwright/.husky/_/husky.sh"
-cd ui_tests_playwright
-npm run format
-npm run lint
+npx run format
+npx run lint
--- ui_tests_playwright/automation/steps/careerPageSteps/CareerSteps.ts
+++ ui_tests_playwright/automation/steps/careerPageSteps/CareerSteps.ts
@@ -10,6 +10,9 @@ class CareerSteps {
 	public async verifyThatCareerWasCreated(careerName: string, searchString: string = careerName) {
 		await driver.executeFunc(async () => {
 			await driver.Page.reload();
+
+
+			
 			await driver.getByTestId(Career.SarchCareerField).clear();
 			await driver.getByTestId(Career.SarchCareerField).fill(searchString);
 			await driver.getByTestId(Career.SearchButton).click();
