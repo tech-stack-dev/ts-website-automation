e9431f8a58646ad0e8f166e7ba9917b949d492be
Daniil <daniil.vostrikov@standardindustries.com>
2023-06-01 20:05:26 +0300

    update script

--- ui_tests_playwright/automation/scripts/GetListOfJirasToSkip.ts
+++ ui_tests_playwright/automation/scripts/GetListOfJirasToSkip.ts
@@ -7,64 +7,66 @@ const token = process.env.JIRA_AUTH_TOKEN;
 const jiraUsername = 'qa_techstack@tech-stack.io';
 
 async function getTicketStatus(ticketKey: string): Promise<string | null> {
-	try {
-		const authToken = Buffer.from(`${jiraUsername}:${token}`).toString('base64');
-		const response = await axios.get(`${jiraBaseUrl}/rest/api/3/issue/${ticketKey}`, {
-			headers: {
-				Authorization: `Basic ${authToken}`,
-			},
-		});
+  try {
+    const authToken = Buffer.from(`${jiraUsername}:${token}`).toString('base64');
+    const response = await axios.get(`${jiraBaseUrl}/rest/api/3/issue/${ticketKey}`, {
+      headers: {
+        Authorization: `Basic ${authToken}`,
+      },
+    });
 
-		const {statusCategory} = response.data.fields.status;
-		return statusCategory.name;
-	} catch (error) {
-		console.error(`Error retrieving status for ticket ${ticketKey}:`, error);
-		return null;
-	}
+    const { statusCategory } = response.data.fields.status;
+    return statusCategory.name;
+  } catch (error) {
+    console.error(`Error retrieving status for ticket ${ticketKey}:`, error);
+    return null;
+  }
 }
 
 async function searchStrings(rootDir: string): Promise<Set<string>> {
-	const foundStrings: Set<string> = new Set();
+  const foundStrings: Set<string> = new Set();
 
-	function searchFiles(directory: string) {
-		const files = fs.readdirSync(directory);
+  function searchFiles(directory: string) {
+    const files = fs.readdirSync(directory);
 
-		for (const file of files) {
-			const filePath = path.join(directory, file);
-			const stat = fs.statSync(filePath);
+    for (const file of files) {
+      const filePath = path.join(directory, file);
+      const stat = fs.statSync(filePath);
 
-			if (stat.isDirectory()) {
-				searchFiles(filePath);
-			} else if (stat.isFile()) {
-				const content = fs.readFileSync(filePath, 'utf-8');
-				const matches = content.matchAll(/TSWEB-[0-9]{1,8}/g);
+      if (stat.isDirectory()) {
+        searchFiles(filePath);
+      } else if (stat.isFile()) {
+        const content = fs.readFileSync(filePath, 'utf-8');
+        const matches = content.matchAll(/TSWEB-[0-9]{1,8}/g);
 
-				for (const match of matches) {
-					foundStrings.add(match[0]);
-				}
-			}
-		}
-	}
+        for (const match of matches) {
+          foundStrings.add(match[0]);
+        }
+      }
+    }
+  }
 
-	searchFiles(rootDir);
-	return foundStrings;
+  searchFiles(rootDir);
+  return foundStrings;
 }
 
 async function run() {
-	const rootDirectory = process.cwd();
-	const foundStrings = await searchStrings(rootDirectory);
+  const rootDirectory = process.cwd();
+  const foundStrings = await searchStrings(rootDirectory);
 
-	const ticketStatuses: string[] = [];
+  const ticketStatuses: string[] = [];
 
-	for (const ticket of foundStrings) {
-		const status = await getTicketStatus(ticket);
-		if (status && !['Ready for QA', 'Testing on Stage', 'UI testing', 'Done', 'Closed'].includes(status)) {
-			ticketStatuses.push(ticket);
-		}
-	}
+  for (const ticket of foundStrings) {
+    const status = await getTicketStatus(ticket);
+    if (status && !['Ready for QA', 'Testing on Stage', 'UI testing', 'Done', 'Closed'].includes(status)) {
+      ticketStatuses.push(ticket);
+    }
+  }
+
+  fs.writeFileSync('ticketStatuses.json', JSON.stringify(ticketStatuses));
 }
 
 run().catch((error) => {
-	console.error('An error occurred:', error);
-	process.exit(1);
+  console.error('An error occurred:', error);
+  process.exit(1);
 });
