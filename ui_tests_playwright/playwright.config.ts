de247d60bb4fefa2aeb9a7ed23fda9cddd9ef290
Anastasiia Liashova <anastasiia.liashova@tech-stack.io>
2023-07-11 22:21:51 +0300

    code fixed

--- ui_tests_playwright/playwright.config.ts
+++ ui_tests_playwright/playwright.config.ts
@@ -1,4 +1,4 @@
-import {PlaywrightTestConfig, devices} from '@playwright/test';
+import {PlaywrightTestConfig} from '@playwright/test';
 import dotenv from 'dotenv';
 import path from 'path';
 
@@ -102,6 +102,7 @@ const config: PlaywrightTestConfig = {
 			use: {
 				channel: 'chrome',
 			},
+			// Delete after https://ts-website.atlassian.net/browse/TSWEB-847
 			grepInvert:[new RegExp("@JobsBlock"), new RegExp("@FilterBlock")],
 		},
 	],
