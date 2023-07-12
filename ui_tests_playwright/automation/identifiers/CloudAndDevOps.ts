0078dcb80fa3c58b2115d7e7e17f5734ac30698c
Valerii-Kryvokhyzha <valerii.kryvokhyzha@tech-stack.io>
2023-07-12 11:27:15 +0300

    Added tests for Cloud & DevOps page

---
+++ ui_tests_playwright/automation/identifiers/CloudAndDevOps.ts
@@ -0,0 +1,12 @@
+export default class CloudAndDevOps {
+	static Info = 'Container-Info';
+	static LeverageCloudDevelopmentServices = 'Container-LeverageCloudDevelopmentServices';
+	static TechnologyStack = 'Container-TechnologyStack';
+	static CaseStudies = 'Container-CaseStudies';
+	static CloudComputingDevelopmentBenefits = 'Container-CloudComputingDevelopmentBenefits';
+	static OurApproachToCloudAppDevelopment = 'Container-OurApproach';
+	static OurCloudDevOpsExperts = 'Container-OurCloudDevOpsExperts';
+	static RelatedServices = 'Container-RelatedServices';
+	static Faq = 'Container-Faq';
+	static RelatedArticles = 'Container-RelatedArticles';
+}
---
+++ ui_tests_playwright/testData/ClutchReviewLinks.ts
@@ -0,0 +1,8 @@
+export default class ClutchReviewLinks {
+	static AnonymousVehicle = 'https://clutch.co/profile/techstack#review-2107015';
+	static DarrenCody = 'https://clutch.co/profile/techstack#review-1961618';
+	static AnonymousPeerToPeer = 'https://clutch.co/profile/techstack#review-1820220';
+	static MarkBeare = 'https://clutch.co/profile/techstack#review-1765062';
+	static DerickDaily = 'https://clutch.co/profile/techstack#review-1634090';
+	static AnonymousMedicalDevice = 'https://clutch.co/profile/techstack#review-1939515';
+}
