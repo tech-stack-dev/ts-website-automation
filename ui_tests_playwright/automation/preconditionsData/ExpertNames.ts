5af96e638f3cbb6304d48195231ba6795d6977d2
Valerii-Kryvokhyzha <valerii.kryvokhyzha@tech-stack.io>
2023-07-21 13:31:50 +0300

    Added tests for Big Data page

---
+++ ui_tests_playwright/automation/preconditionsData/ExpertNames.ts
@@ -0,0 +1,6 @@
+export enum ExpertNames {
+	OleksiiSvystun = 'Oleksii Svystun',
+	YevheniiKarachevtsev = 'Yevhenii Karachevtsev',
+	OleksandrBezrukov = 'Oleksandr Bezrukov',
+	IvanYeremenko = 'Ivan Yeremenko',
+}
