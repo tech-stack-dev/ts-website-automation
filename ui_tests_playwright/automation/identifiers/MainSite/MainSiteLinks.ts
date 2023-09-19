3f1752c3021dd8c7e2f2d8db9d79662b4e034efc
Valerii-Kryvokhyzha <valerii.kryvokhyzha@tech-stack.io>
2023-09-19 17:35:22 +0300

    Updated tests for UiUx page

--- ui_tests_playwright/automation/identifiers/MainSite/MainSiteLinks.ts
+++ ui_tests_playwright/automation/identifiers/MainSite/MainSiteLinks.ts
@@ -1,8 +1,8 @@
 export default class MainSiteLinks {
 	static Tiktok = 'Link-TikTok';
 	static Instagram = 'Link-Instagram';
-
 	static Clutch = 'Link-Clutch';
+	static CaseStudy = 'Link-CaseStudy';
 
 	// Links to industries pages
 	static Healthcare = 'Link-Healthcare';
