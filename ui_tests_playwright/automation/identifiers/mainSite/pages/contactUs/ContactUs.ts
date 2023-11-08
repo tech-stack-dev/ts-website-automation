a77cfc8b482c7e37f26e08889b7ea78f017c1008
Kateryna Kotliarova <Kateryna.Kotliarova@juriba.com>
2023-11-08 13:54:52 +0200

    add new test for Contact Us

--- ui_tests_playwright/automation/identifiers/mainSite/pages/contactUs/ContactUs.ts
+++ ui_tests_playwright/automation/identifiers/mainSite/pages/contactUs/ContactUs.ts
@@ -1,6 +1,7 @@
 import GeneralContainersMainSite from '../../GeneralContainersMainSite';
 
 export default class ContactUs extends GeneralContainersMainSite {
+	static Title = 'Title';
 	static GetInTouchForm = 'Container-GetInTouchForm';
 	static Cooperation = 'Container-Cooperation';
 }
--- ui_tests_playwright/automation/test/ui/mainSite/contactUs/ContactUsPage.test.ts
+++ ui_tests_playwright/automation/test/ui/mainSite/contactUs/ContactUsPage.test.ts
@@ -10,6 +10,11 @@ test.beforeEach(async () => {
 	await baseDriverSteps.createsNewBrowserAndGoToUrl(UrlProvider.urlBuilder(UrlPath.ContactUs));
 });
 
+test('Check the Title from the "Contact Us" page @Regression @ContactUs @TSWEB-1082', async () => {
+	const title = driver.getByTestId(ContactUs.Title);
+	await expect(title.getByTestId(ContactUs.Title)).toHaveText('Contact Our Team of Experts');
+});
+
 test('Check the container title and number from the "Contact Us" page @Regression @ContactUs @TSWEB-148 @TSWEB-1082', async () => {
 	const containers = [driver.getByTestId(ContactUs.GetInTouch), driver.getByTestId(ContactUs.Cooperation)];
 
