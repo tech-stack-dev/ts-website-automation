aa0ba397fcb776f7b24999e43d92d621b90394e1
Kateryna Kotliarova <Kateryna.Kotliarova@juriba.com>
2023-11-28 14:50:38 +0200

    Added tests for Terms of use page

---
+++ ui_tests_playwright/automation/identifiers/mainSite/pages/CookiePolicy.ts
@@ -0,0 +1,15 @@
+import GeneralContainersMainSite from '../GeneralContainersMainSite';
+
+export default class CookiePolicy extends GeneralContainersMainSite {
+	static GlossaryBasicConcepts = 'Container-GlossaryBasicConcepts';
+	static WhosePersonalDataDoWeCollect = 'Container-WhosePersonalDataDoWeCollect';
+	static WhenMayWeProcessYourPersonalData = 'Container-WhenMayWeProcessYourPersonalData';
+	static WhatPersonalDataDoWeCollect = 'Container-WhatPersonalDataDoWeCollect';
+	static HowLongDoWeProcessYourData = 'Container-HowLongDoWeProcessYourData';
+	static WhoHasAccessToYourPersonalData = 'Container-WhoHasAccessToYourPersonalData';
+	static WhatAreCookieFiles = 'Container-WhatAreCookieFiles';
+	static TheCookiesWeUse = 'Container-TheCookiesWeUse';
+	static HowToManageCookies = 'Container-HowToManageCookies';
+	static ContactDetailsOfTechstack = 'Container-ContactDetailsOfTechstack';
+	static WhatAreYourRightsRegardingProcessing = 'Container-WhatAreYourRightsRegardingProcessing';
+}
---
+++ ui_tests_playwright/automation/test/ui/mainSite/TermsOfUse.test.ts
@@ -0,0 +1,42 @@
+import {expect, test} from '@playwright/test';
+import {baseDriverSteps} from '../../../base/step/BaseDriverSteps';
+import UrlProvider from '../../../providers/UrlProvider';
+import {driver} from '../../../base/driver/Driver';
+import Container from '../../../identifiers/Container';
+import UrlPath from '../../../providers/UrlPath';
+import TermsOfUse from '../../../identifiers/mainSite/pages/TermsOfUse';
+
+test.beforeEach(async () => {
+	await baseDriverSteps.createsNewBrowserAndGoToUrl(UrlProvider.urlBuilder(UrlPath.Terms));
+});
+
+test('Check title from the "Terms of use" page @Regression @TermsOfUse @TSWEB-1186', async () => {
+	const infoContainer = driver.getByTestId(TermsOfUse.Info);
+	await expect(infoContainer.getByTestId(Container.Title)).toHaveText('Terms of use');
+});
+
+test('Check the container title and number from the "Terms of use" page @Regression @TermsOfUse @TSWEB-1186', async () => {
+	const containers = [
+		driver.getByTestId(TermsOfUse.TermsOfUseOfTheWebsite),
+		driver.getByTestId(TermsOfUse.TheUseOfTheSite),
+		driver.getByTestId(TermsOfUse.PersonalInformationOfTheUser),
+		driver.getByTestId(TermsOfUse.LimitationOfLiability),
+		driver.getByTestId(TermsOfUse.ProcedureOfTheAgreement),
+		driver.getByTestId(TermsOfUse.ExclusiveRights),
+	];
+
+	const expectedData = [
+		['1. Terms of use\n of the website', '02'],
+		['2. The use of \nthe Site', '03'],
+		['3. Personal information \nof the User', '04'],
+		['4. Limitation \nof liability', '05'],
+		['5. Procedure of the Agreement', '06'],
+		['6. Exclusive \nRights', '07'],
+	];
+
+	await baseDriverSteps.checkContainerTitlesAndNumbers(containers, expectedData);
+});
+
+test.afterEach(async () => {
+	await driver.closeDrivers();
+});
