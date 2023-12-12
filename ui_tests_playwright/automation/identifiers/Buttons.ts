0667bb41c6b49e2ee2d15c31920e352359bbaf27
Valerii-Kryvokhyzha <valerii.kryvokhyzha@tech-stack.io>
2023-12-12 15:00:40 +0200

    Moved button ids from Buttons class to Header

--- ui_tests_playwright/automation/identifiers/Buttons.ts
+++ ui_tests_playwright/automation/identifiers/Buttons.ts
@@ -1,34 +1,6 @@
 export default class Buttons {
-	// Header components
-	static Menu = 'HeaderOpenMenuButton-HeaderOpenMenuButton';
-	static HeaderLogo = 'HeaderLogo-HeaderLogo';
 	static ContactUs = 'Button-ContactUs';
-
-	// Menu components
-	static Close = 'CloseButton-Close';
-	static Menu_Services = 'ServicesHeader-ServicesHeader';
-	static Menu_Company = 'CompanyHeader-CompanyHeader';
-	static Menu_ContactUs = 'ContactUsHeader-ContactUsHeader';
-
-	static Services_OurServices = 'Services-OurServices';
-	static Services_CustomDev = 'Services-CustomDev';
-	static Services_DigitalTransform = 'Services_DigitalTransformation';
-	static Services_CloudDev = 'Services-CloudDev';
-	static Services_MobileDev = 'Services-MobileDev';
-	static Services_BigData = 'Services-BigData';
-	static Services_InternetOfThings = 'Services-InternetOfThings';
-	static Services_DevOps = 'Services-DevOps';
-	static Services_AiDevelopment = 'Services-AIML';
-	static Services_UiUxDesign = 'Services-UiUxDesign';
-	static Services_QaAsAServ = 'Services-QaAsAServ';
-	static Services_ConsultingServ = 'Services-ConsultingServ';
-
-	static Company_AboutUs = 'Company-AboutUs';
-	static Company_HowWeWork = 'Company-HowWeWork';
-	static Company_Career = 'Company-Career';
-	static Company_CaseStudies = 'Company-CaseStudies';
-	static Company_Blog = 'Company-Blog';
-
+	
 	// Social links
 	static LinkedIn = 'Button-LinkedIn';
 	static Facebook = 'Button-Facebook';
---
+++ ui_tests_playwright/automation/identifiers/mainSite/Header.ts
@@ -0,0 +1,36 @@
+export default class Header {
+	static Container_Header = 'Container-Header'
+
+	static Menu = 'HeaderOpenMenuButton-HeaderOpenMenuButton';
+	static Close = 'CloseButton-Close';
+
+	static Logo = 'HeaderLogo-HeaderLogo';
+	static Industries = 'Header-Industries';
+	static Services = 'Header-Services';
+	static Company = 'Header-Company';
+	static Pricing = 'Header-Pricing';
+
+	// Menu components
+	static Industries_Healthcare = 'Industries-Healthcare';
+	static Industries_TransportationAndLogistics= 'Industries-TransportationAndLogistics';
+	static Industries_RenewableEnergy = 'Industries-RenewableEnergy';
+
+	static Services_OurServices = 'Services-OurServices';
+	static Services_CustomDev = 'Services-CustomDev';
+	static Services_DigitalTransform = 'Services_DigitalTransformation';
+	static Services_CloudDev = 'Services-CloudDev';
+	static Services_MobileDev = 'Services-MobileDev';
+	static Services_BigData = 'Services-BigData';
+	static Services_InternetOfThings = 'Services-InternetOfThings';
+	static Services_DevOps = 'Services-DevOps';
+	static Services_AiDevelopment = 'Services-AIML';
+	static Services_UiUxDesign = 'Services-UiUxDesign';
+	static Services_QaAsAServ = 'Services-QaAsAServ';
+	static Services_ConsultingServ = 'Services-ConsultingServ';
+
+	static Company_AboutUs = 'Company-AboutUs';
+	static Company_HowWeWork = 'Company-HowWeWork';
+	static Company_Career = 'Company-Career';
+	static Company_CaseStudies = 'Company-CaseStudies';
+	static Company_Blog = 'Company-Blog';
+}
