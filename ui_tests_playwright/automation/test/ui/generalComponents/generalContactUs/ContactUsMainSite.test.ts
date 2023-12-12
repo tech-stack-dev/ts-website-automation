2bad04ced799c8c09b4702e7bbec36e0be5c126a
Valerii-Kryvokhyzha <valerii.kryvokhyzha@tech-stack.io>
2023-12-12 15:26:08 +0200

    Upated tests for ContactUs button

--- ui_tests_playwright/automation/test/ui/generalComponents/generalContactUs/ContactUsMainSite.test.ts
+++ ui_tests_playwright/automation/test/ui/generalComponents/generalContactUs/ContactUsMainSite.test.ts
@@ -1,4 +1,4 @@
-import {test} from '@playwright/test';
+import {test, expect, Locator} from '@playwright/test';
 import {driver} from '../../../../base/driver/Driver';
 import {baseDriverSteps} from '../../../../base/step/BaseDriverSteps';
 import Buttons from '../../../../identifiers/Buttons';
@@ -6,9 +6,17 @@ import UrlPath from '../../../../providers/UrlPath';
 import UrlProvider from '../../../../providers/UrlProvider';
 import {serviceUrl, companyUrl, industryUrl} from '../../../../preconditionsData/UrlPreconditions';
 import {CompanyEnum} from '../../../../enum/CompanyEnum';
+import {ColorsEnum} from '../../../../enum/ColorsEnum';
+import Header from '../../../../identifiers/mainSite/Header';
+import {locatorUtils} from '../../../../utils/LocatorUtils';
+
+let header: Locator;
+let contactUsButton: Locator;
 
 test.beforeEach(async () => {
 	await baseDriverSteps.createsNewBrowser();
+	header = driver.getByTestId(Header.Container_Header);
+	contactUsButton = header.getByTestId(Buttons.ContactUs);
 });
 
 test('Check "Contact Us" button from "Main", "Contact Us", and "Pricing" pages @Regression @ContactUs @TSWEB-532', async () => {
@@ -20,7 +28,7 @@ test('Check "Contact Us" button from "Main", "Contact Us", and "Pricing" pages @
 
 	for (const url of urlList) {
 		await baseDriverSteps.goToUrl(url);
-		await driver.getByTestId(Buttons.ContactUs).click();
+		await contactUsButton.click();
 		await baseDriverSteps.checkUrl(UrlProvider.urlBuilder(UrlPath.ContactUs));
 	}
 });
@@ -28,7 +36,7 @@ test('Check "Contact Us" button from "Main", "Contact Us", and "Pricing" pages @
 test('Check "Contact Us" button from "Industries" pages @Regression @ContactUs', async () => {
 	for (const url of Object.values(industryUrl)) {
 		await baseDriverSteps.goToUrl(url);
-		await driver.getByTestId(Buttons.ContactUs).click();
+		await contactUsButton.click();
 		await baseDriverSteps.checkUrl(UrlProvider.urlBuilder(UrlPath.ContactUs));
 	}
 });
@@ -36,7 +44,7 @@ test('Check "Contact Us" button from "Industries" pages @Regression @ContactUs',
 test('Check "Contact Us" button from "Services" pages @Regression @ContactUs @TSWEB-532', async () => {
 	for (const url of Object.values(serviceUrl)) {
 		await baseDriverSteps.goToUrl(url);
-		await driver.getByTestId(Buttons.ContactUs).click();
+		await contactUsButton.click();
 		await baseDriverSteps.checkUrl(UrlProvider.urlBuilder(UrlPath.ContactUs));
 	}
 });
@@ -51,7 +59,7 @@ test('Check "Contact Us" button from "Company" pages @Regression @ContactUs @TSW
 	];
 	for (const url of urlList) {
 		await baseDriverSteps.goToUrl(url);
-		await driver.getByTestId(Buttons.ContactUs).click();
+		await contactUsButton.click();
 		await baseDriverSteps.checkUrl(UrlProvider.urlBuilder(UrlPath.ContactUs));
 	}
 });
@@ -64,10 +72,42 @@ test('Check "Contact Us" button from "Terms of use", "Cookie Policy", and "Sitem
 	];
 	for (const url of urlList) {
 		await baseDriverSteps.goToUrl(url);
-		await driver.getByTestId(Buttons.ContactUs).click();
+		await contactUsButton.click();
 		await baseDriverSteps.checkUrl(UrlProvider.urlBuilder(UrlPath.ContactUs));
 	}
 });
+
+const urlList: Array<string> = [
+	UrlProvider.webSiteUrl(),
+	companyUrl[CompanyEnum.AboutUs],
+	companyUrl[CompanyEnum.HowWeWork],
+	companyUrl[CompanyEnum.CaseStudies],
+	companyUrl[CompanyEnum.Pricing],
+	UrlProvider.urlBuilder(UrlPath.ContactUs),
+	UrlProvider.urlBuilder(UrlPath.Whitepapers),
+	UrlProvider.urlBuilder(UrlPath.Terms),
+	UrlProvider.urlBuilder(UrlPath.CookiesPolicy),
+	UrlProvider.urlBuilder(UrlPath.Sitemap),
+].concat(Object.values(industryUrl).concat(Object.values(serviceUrl)));
+
+for (const url of urlList) {
+	test(`Check "Contact Us" button color on the "${url}" page @Regression @ContactUs @TSWEB-532`, async () => {
+		await baseDriverSteps.goToUrl(url);
+		expect(await locatorUtils.checkBackgroundColor(contactUsButton, ColorsEnum.Yellow_FFC600)).toBeTruthy();
+	});
+
+	test(`Check "Contact Us" button color after hovering on the "${url}" page @Regression @ContactUs @TSWEB-532`, async () => {
+		await baseDriverSteps.goToUrl(url);
+
+		await contactUsButton.hover();
+		// Wait for changing the color
+		await driver.Page.waitForTimeout(1000);
+		const actualColor = await contactUsButton.evaluate(async (el) => {
+			return getComputedStyle(el).backgroundColor;
+		});
+		expect(actualColor).toBe(ColorsEnum.Yellow_Hover_EDAB00);
+	});
+}
 
 test.afterEach(async () => {
 	await driver.closeDrivers();
