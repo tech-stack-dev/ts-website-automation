655f5ea0f888a77e9454d61bf7bed9bfbbeec96e
Valerii-Kryvokhyzha <valerii.kryvokhyzha@tech-stack.io>
2023-12-15 13:43:00 +0200

    Decreased workers to 12)

--- ui_tests_playwright/playwright.config.ts
+++ ui_tests_playwright/playwright.config.ts
@@ -45,7 +45,7 @@ const config: PlaywrightTestConfig = {
 	/* Retry on CI only */
 	retries: 2,
 	/* Opt out of parallel tests on CI. */
-	workers: 16,
+	workers: 12,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: [
 		['html', {open: 'never'}],
