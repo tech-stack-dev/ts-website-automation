f119da52311d6f77ceea8dbdd373df1ec0661bbe
Daniil <daniil.vostrikov@standardindustries.com>
2023-12-20 15:05:55 +0200

    update script

--- ui_tests_playwright/automation/scripts/GetAutotestsCount.ts
+++ ui_tests_playwright/automation/scripts/GetAutotestsCount.ts
@@ -1,24 +1,19 @@
 import * as fs from 'fs';
 
-// Чтение содержимого файла
 const content = fs.readFileSync('test-output.txt', 'utf-8');
 
-// Нахождение чисел перед "passed", "failed", "flaky" и "skipped"
 const passedMatches = content.match(/(\d+) passed/);
 const failedMatches = content.match(/(\d+) failed/);
 const flakyMatches = content.match(/(\d+) flaky/);
 const skippedMatches = content.match(/(\d+) skipped/);
 
-// Извлечение найденных чисел
 const passed = passedMatches ? Number(passedMatches[1]) : 0;
 const failed = failedMatches ? Number(failedMatches[1]) : 0;
 const flaky = flakyMatches ? Number(flakyMatches[1]) : 0;
 const skipped = skippedMatches ? Number(skippedMatches[1]) : 0;
 
-// Вычисление общего числа тестов
 const total = passed + failed + flaky + skipped;
 
-// Вывод результатов в формате, понятном GitHub Actions
 console.log(`::set-env name=PASSED::${passed}`);
 console.log(`::set-env name=FAILED::${failed}`);
 console.log(`::set-env name=FLAKY::${flaky}`);
