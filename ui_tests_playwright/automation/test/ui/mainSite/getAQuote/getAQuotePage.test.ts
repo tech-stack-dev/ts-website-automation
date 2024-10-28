50b1aa6d965f177dc79e33d30b584caecd1fd191
SofiiaRostkovych <sofiia.rostkovych@tech-stack.io>
2024-10-28 16:49:59 +0200

    Add check for desktop display of last logo

--- ui_tests_playwright/automation/test/ui/mainSite/getAQuote/getAQuotePage.test.ts
+++ ui_tests_playwright/automation/test/ui/mainSite/getAQuote/getAQuotePage.test.ts
@@ -12,37 +12,38 @@ test.beforeEach(async () => {
 	await baseDriverSteps.createsNewBrowserAndGoToUrl(UrlProvider.urlBuilder(UrlPath.GetAQuote));
 });
 
-test('Check the container title and number from the "Get a Quote" page @desktop @mobile @Regression @GetAQuote @TSWEB-1766',
-	async () => {
-		const containers = [driver.getByTestId(GetAQuote.OurProposal), driver.getByTestId(GetAQuote.TrustedBy)];
+test('Check the container title and number from the "Get a Quote" page @desktop @mobile @Regression @GetAQuote @TSWEB-1766', async () => {
+	const containers = [driver.getByTestId(GetAQuote.OurProposal), driver.getByTestId(GetAQuote.TrustedBy)];
 
-		const expectedData = [
-			["What You'll Get with Our Proposal:", '01'],
-			['Trusted By', '02'],
-		];
+	const expectedData = [
+		["What You'll Get with Our Proposal:", '01'],
+		['Trusted By', '02'],
+	];
 
-		await baseDriverSteps.checkContainerTitlesAndNumbers(containers, expectedData);
-	}
-);
+	await baseDriverSteps.checkContainerTitlesAndNumbers(containers, expectedData);
+});
 
-test('Check partner logos in "Trusted By" container from the "Get a Quote" page @desktop @mobile @Regression @GetAQuote @TSWEB-1766',
-	async () => {
-		const trustedByContainer = driver.getByTestId(GetAQuote.TrustedBy);
-		await trustedByContainer.scrollIntoViewIfNeeded();
-		const partnerLogos = [MainSiteImages.Corel, MainSiteImages.SkillLabLogo, MainSiteImages.VivaQuantLogo, MainSiteImages.ExaumLogo];
+test('Check partner logos in "Trusted By" container from the "Get a Quote" page @desktop @mobile @Regression @GetAQuote @TSWEB-1766', async () => {
+	const trustedByContainer = driver.getByTestId(GetAQuote.TrustedBy);
+	await trustedByContainer.scrollIntoViewIfNeeded();
+	const partnerLogos = [
+		MainSiteImages.Corel,
+		MainSiteImages.SkillLabLogo,
+		MainSiteImages.VivaQuantLogo,
+		MainSiteImages.ExaumLogo,
+	];
 
-		partnerLogos.forEach(async logo => {
-			await expect(trustedByContainer.getByTestId(logo)).toBeVisible();
-		});
-		
-		const lastLogo = await containerSteps.getDynamicLocator({
-			desktopLocator: MainSiteImages.RuckifyLogo,
-            mobileLocator: partnerLogos[partnerLogos.length-1]
-		});
+	partnerLogos.forEach(async (logo) => {
+		await expect(trustedByContainer.getByTestId(logo)).toBeVisible();
+	});
 
-		await expect(lastLogo).toBeVisible();
-	}
-);
+	const lastLogo = await containerSteps.getDynamicLocator({
+		desktopLocator: MainSiteImages.RuckifyLogo,
+		mobileLocator: partnerLogos[partnerLogos.length - 1],
+	});
+
+	await expect(lastLogo).toBeVisible();
+});
 
 test.afterEach(async () => {
 	await driver.closeDrivers();
