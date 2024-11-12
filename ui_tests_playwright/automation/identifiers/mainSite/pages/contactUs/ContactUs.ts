3fea3890fcdf3454009c82a49bf2f450c9dc1a1c
AnnaLysokhmara <anna.lysokhmara@tech-stack.io>
2024-11-12 16:43:54 +0200

    Update locators

--- ui_tests_playwright/automation/identifiers/mainSite/pages/contactUs/ContactUs.ts
+++ ui_tests_playwright/automation/identifiers/mainSite/pages/contactUs/ContactUs.ts
@@ -3,6 +3,5 @@ import GeneralContainersMainSite from '../../GeneralContainersMainSite';
 export default class ContactUs extends GeneralContainersMainSite {
 	static GetInTouchForm = 'Container-GetInTouchForm';
 	static ContactUs = 'Container-ContactUs';
-	static ConsultWithUs = 'Container-ConsultWithUs';
 	static Cooperation = 'Container-Cooperation';
 }
