6171c68b8ff4405b566b47b54ec8345f48109196
AnnaLysokhmara <anna.lysokhmara@tech-stack.io>
2024-11-24 14:27:44 +0200

    Remove CalendlyInFormsBusiness.test waiting for data-id

--- ui_tests_playwright/automation/steps/ui/CalendlySteps.ts
+++ ui_tests_playwright/automation/steps/ui/CalendlySteps.ts
@@ -41,36 +41,6 @@ class CalendlySteps {
 		await driver.locator(Calendly.close).click();
 		await expect(driver.locator(Calendly.frame)).toBeHidden();
 	}
-
-	public async checkMemberCardCalendlyInForm(
-		memberCard: Locator | undefined,
-		memberData: {name: string; role: string}
-	) {
-		if (!memberCard) {
-			throw new Error(`No card found for expert: ${memberData.name}`);
-		}
-		await expect(memberCard).toBeVisible();
-
-		const nameByTestId = memberCard.getByTestId(Container.MemberName);
-		const nameByClass = memberCard.locator('.h3.form-expert-name.opacity-1');
-		const name = (await nameByTestId.count()) > 0 ? nameByTestId : nameByClass;
-
-		const roleByTestId = memberCard.getByTestId(Container.MemberRole);
-		const roleByClass = memberCard.locator('.h4.form-expert-position.opacity-1');
-		const role = (await nameByTestId.count()) > 0 ? roleByTestId : roleByClass;
-
-		const consultButton = memberCard.getByTestId(MainSiteButtons.ScheduleAConsultationInCalendly);
-
-		const imageInBlock = memberCard.locator('.member-foto');
-		const imageInFormSection = memberCard.locator('.expert-avatar');
-		const image = (await imageInBlock.count()) > 0 ? imageInBlock : imageInFormSection;
-
-		await expect(name).toHaveText(memberData.name);
-		await expect(role).toHaveText(memberData.role);
-		await expect(consultButton).toBeVisible();
-		await expect(consultButton).toBeEnabled();
-		await baseDriverSteps.checkImagesVisibility(image, 1);
-	}
 }
 
 const calendlySteps = new CalendlySteps();
