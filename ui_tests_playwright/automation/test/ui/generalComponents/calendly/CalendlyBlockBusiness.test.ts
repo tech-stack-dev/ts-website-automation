5d5f1730515d56547be4411bf2f0b04e3f9c8d0b
AnnaLysokhmara <anna.lysokhmara@tech-stack.io>
2024-11-25 15:19:21 +0200

    Refactor Calendly test with one  container identifier instead of two

--- ui_tests_playwright/automation/test/ui/generalComponents/calendly/CalendlyBlockBusiness.test.ts
+++ ui_tests_playwright/automation/test/ui/generalComponents/calendly/CalendlyBlockBusiness.test.ts
@@ -1,16 +1,13 @@
 import {expect, Locator, test} from '@playwright/test';
 import {baseDriverSteps} from '../../../../base/step/BaseDriverSteps';
 import {driver} from '../../../../base/driver/Driver';
-import GetAQuote from '../../../../identifiers/mainSite/pages/getAQuote/GetAQuote';
 import UrlPath from '../../../../providers/UrlPath';
 import UrlProvider from '../../../../providers/UrlProvider';
 import Container from '../../../../identifiers/Container';
 import {calendlySteps} from '../../../../steps/ui/CalendlySteps';
-import ContactUs from '../../../../identifiers/mainSite/pages/contactUs/ContactUs';
+import GeneralContainersMainSite from '../../../../identifiers/mainSite/GeneralContainersMainSite';
 
-let getAQuoteConsultWithUsContainer: Locator;
-let contactUsConsultWithUsContainer: Locator;
-let consultWithUsContainerList: Locator[];
+let consultWithUsContainer: Locator;
 
 const testDataProvider: string[] = [
 	UrlProvider.urlBuilder(UrlPath.GetAQuote),
@@ -19,31 +16,27 @@ const testDataProvider: string[] = [
 
 test.beforeEach(async () => {
 	await baseDriverSteps.createsNewBrowser();
-	getAQuoteConsultWithUsContainer = driver.getByTestId(GetAQuote.ConsultWithUs);
-	contactUsConsultWithUsContainer = driver.getByTestId(ContactUs.ConsultWithUs);
-	consultWithUsContainerList = [getAQuoteConsultWithUsContainer, contactUsConsultWithUsContainer];
+	consultWithUsContainer = driver.getByTestId(GeneralContainersMainSite.ConsultWithUs);
 });
 
 test('Check member cards from from Calendly block on the "Contact us" and "Get a Quote" pages @desktop @mobile @Regression @Calendly @TSWEB-1852', async () => {
 	for (const url of testDataProvider) {
 		await baseDriverSteps.goToUrl(url);
 
-		for (const consultWithUsContainer of consultWithUsContainerList) {
-			const cardElements = await consultWithUsContainer.getByTestId(Container.MemberCard).all();
+		const cardElements = await consultWithUsContainer.getByTestId(Container.MemberCard).all();
 
-			const allExperts = [
-				{name: 'Max Levytskyi', role: 'Managing Partner'},
-				{name: 'Anton Ivanchenko', role: 'Business Development Manager'},
-				{name: 'Artem Marynych', role: 'Chief Growth Officer'},
-			];
+		const allExperts = [
+			{name: 'Max Levytskyi', role: 'Managing Partner'},
+			{name: 'Anton Ivanchenko', role: 'Business Development Manager'},
+			{name: 'Artem Marynych', role: 'Chief Growth Officer'},
+		];
 
-			expect(cardElements.length).toBe(allExperts.length);
+		expect(cardElements.length).toBe(allExperts.length);
 
-			for (const expertData of allExperts) {
-				const matchingCard = await calendlySteps.findMatchingMemberCardByName(cardElements, expertData.name);
-				await calendlySteps.checkMemberCardCalendly(matchingCard, expertData);
-				await calendlySteps.checkAppropriateCalendlyModalOpensAndCloses(matchingCard);
-			}
+		for (const expertData of allExperts) {
+			const matchingCard = await calendlySteps.findMatchingMemberCardByName(cardElements, expertData.name);
+			await calendlySteps.checkMemberCardCalendly(matchingCard, expertData);
+			await calendlySteps.checkAppropriateCalendlyModalOpensAndCloses(matchingCard);
 		}
 	}
 });
