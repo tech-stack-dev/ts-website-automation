412ff63c34e2011ab684820fa6cb6f835fe2ec58
Veronika Dolbina <veronika.dolbina@tech-stack.io>
2025-04-07 18:18:16 +0300

    deleted pages that are already redesigned on Webflow

--- ui_tests_playwright/automation/identifiers/mainSite/pages/services/CloudDevelopment.ts
+++
@@ -1,7 +0,0 @@
-import GeneralContainersMainSite from '../../GeneralContainersMainSite';
-
-export default class CloudDevelopment extends GeneralContainersMainSite {
-	static LeverageCloudNativeDevServ = 'Container-LeverageCloudDevelopmentServices';
-	static CloudComputingDevelopmentBenefits = 'Container-CloudComputingDevelopmentBenefits';
-	static OurLeadingCloudExperts = 'Container-OurCloudDevOpsExperts';
-}
